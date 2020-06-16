a8996baf1d014013cf2c602b2d968a5ffd0f0a8f
Gaurav Roy <gauravroy@Gauravs-MacBook-Pro.local>
2020-06-16 22:30:57 +0530

    feat made with react native and server is updated for such

--- server.js
+++ server.js
@@ -1,64 +1,67 @@
+const express = require("express");
 
-const express = require('express')
+var io = require("socket.io")({
+  path: "/io/webrtc"
+});
 
-var io = require('socket.io')
-({
-  path: '/webrtc'
-})
-
-const app = express()
-const port = 8080
+const app = express();
+const port = 8080;
 
 // app.get('/', (req, res) => res.send('Hello World!!!!!'))
 
 //https://expressjs.com/en/guide/writing-middleware.html
-app.use(express.static(__dirname + '/build'))
-app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/build/index.html')
-})
+app.use(express.static(__dirname + "/build"));
+app.get("/", (req, res, next) => {
+  res.sendFile(__dirname + "/build/index.html");
+});
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
 
-io.listen(server)
+io.listen(server);
+
+// default namespace
+io.on("connection", socket => {
+  console.log("connected");
+});
 
 // https://www.tutorialspoint.com/socket.io/socket.io_namespaces.htm
-const peers = io.of('/webrtcPeer')
+const peers = io.of("/webrtcPeer");
 
 // keep a reference of all socket connections
-let connectedPeers = new Map()
+let connectedPeers = new Map();
 
-peers.on('connection', socket => {
+peers.on("connection", socket => {
+  console.log(socket.id);
+  socket.emit("connection-success", { success: socket.id });
 
-  console.log(socket.id)
-  socket.emit('connection-success', { success: socket.id })
+  connectedPeers.set(socket.id, socket);
 
-  connectedPeers.set(socket.id, socket)
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+    connectedPeers.delete(socket.id);
+  });
 
-  socket.on('disconnect', () => {
-    console.log('disconnected')
-    connectedPeers.delete(socket.id)
-  })
-
-  socket.on('offerOrAnswer', (data) => {
+  socket.on("offerOrAnswer", data => {
     // send to the other peer(s) if any
     for (const [socketID, socket] of connectedPeers.entries()) {
       // don't send to self
       if (socketID !== data.socketID) {
-        console.log(socketID, data.payload.type)
-        socket.emit('offerOrAnswer', data.payload)
+        console.log(socketID, data.payload.type);
+        socket.emit("offerOrAnswer", data.payload);
       }
     }
-  })
+  });
 
-  socket.on('candidate', (data) => {
+  socket.on("candidate", data => {
     // send candidate to the other peer(s) if any
     for (const [socketID, socket] of connectedPeers.entries()) {
       // don't send to self
       if (socketID !== data.socketID) {
-        console.log(socketID, data.payload)
-        socket.emit('candidate', data.payload)
+        console.log(socketID, data.payload);
+        socket.emit("candidate", data.payload);
       }
     }
-  })
-
-})
+  });
+});
